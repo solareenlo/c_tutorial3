0d46d8b3e4d0fb9f4d834fc7cd7bddc5b97515f6
solareenlo <solareenlo@gmail.com>
2018-09-16 21:25:32 +0900

    ポインタのポインタを使って、main関数の外で動的メモリの確保と解放を行う

---
+++ advanced3/pointer_pointer_func.c
@@ -0,0 +1,37 @@
+// ポインタのポインタを使って、関数で動的メモリ確保と解放
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int createMemory (char**, int);
+void freeMemory (char**);
+
+int main(void) {
+    char* s = NULL;
+
+    createMemory(&s, 255);
+
+    strcpy(s, "HelloWorld\n");
+    printf("%s", s);
+
+    freeMemory(&s);
+
+    return 0;
+}
+
+int createMemory(char** mem, int size) {
+    if(*mem == NULL) {
+        *mem = (char*)malloc(sizeof(char)*size);
+        if(*mem == 0) {
+            return -1; // メモリが生成できなければエラー
+        }
+        return 0;
+    }
+    return -1; // 与えられたメモリの値が不定ならば、エラー
+}
+
+void freeMemory(char** mem) {
+    if(mem != 0) {
+        free(*mem);
+    }
+}
