e2d5ec9d97ab3cef6f5990bf0a0dcf65d731433e
solareenlo <solareenlo@gmail.com>
2018-09-17 12:32:49 +0900

    文字列関数(文字列から数値への変換)の練習

---
+++ advanced4/string3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char s1[] = "1000";
+    char s2[] = "12.345";
+    int a;
+    double b;
+
+    a = atoi(s1);
+    b = atoi(s2);
+
+    printf("a = %d, b = %f\n", a, b);
+
+    return 0;
+}
