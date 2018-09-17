c380114997b7ae65ebe461539400edf89fcdc68a
solareenlo <solareenlo@gmail.com>
2018-09-17 16:55:03 +0900

    文字列として定義された100を整数の値として出力

---
+++ advanced4/prob/prob5_7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char numString[] = "100";
+    int num;
+
+    num = atoi(numString);
+
+    printf("%d\n", num);
+
+    return 0;
+}
