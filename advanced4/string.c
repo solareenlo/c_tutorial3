293ad39a804522b481e7038df88caaa310b88d77
solareenlo <solareenlo@gmail.com>
2018-09-17 12:19:28 +0900

    文字列関数(文字列のコピー・結合・長さ)の練習

--- advanced4/string.c
+++ advanced4/string.c
@@ -5,13 +5,13 @@ int main(void) {
     char s[10];
     int len;
 
-    strcpy(s, "ABC");
+    strcpy(s, "ABC"); // 右辺の文字列を左辺の文字列変数にコピー
     printf("s = %s\n", s);
 
-    strcat(s, "def");
+    strcat(s, "def"); // 右辺の文字列を左辺の文字列変数に追加
     printf("s = %s\n", s);
 
-    len = strlen(s);
+    len = strlen(s); // 引数の文字列の長さを返す
     printf("文字列の長さ：%d\n", len);
 
     return 0;
