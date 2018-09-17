1c864736d9883e1b642b71f96f4470ef112a7897
solareenlo <solareenlo@gmail.com>
2018-09-17 12:22:54 +0900

    文字列関数(文字列の比較)の練習

---
+++ advanced4/string2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char s1[256], s2[256];
+
+    printf("s1 = ");
+    scanf("%s", s1);
+
+    printf("s2 = ");
+    scanf("%s", s2);
+
+    if(strcmp(s1, s2) == 0) {
+        printf("s1とs2は等しいです\n");
+    } else {
+        printf("s1とs2は等しくないです\n");
+    }
+
+    return 0;
+}
